51919d1cdfef0779cab1e836cc6f99aa0c3ab47e
cuhte3 <farid@getportal.co>
2023-11-25 17:25:38 +0700

    Create Network.swift

---
+++ Sources/SwiftBTC/Network.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftBTC
+//
+//  Created by Otto Suess on 05.04.18.
+//  Copyright © 2018 Zap. All rights reserved.
+//
+
+import Foundation
+
+public enum Network: String, Codable, CaseIterable {
+    case regtest
+    case testnet
+    case mainnet
+    case simnet
+}
