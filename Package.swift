ef7c2e13b771aeef02776e10b97351a77faf270a
cuhte3 <farid@getportal.co>
2023-11-25 17:28:38 +0700

    Update Package.swift
    Added BigInt as dependency

--- Package.swift
+++ Package.swift
@@ -5,19 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftBTC",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .macCatalyst(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftBTC",
             targets: ["SwiftBTC"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/attaswift/BigInt", .upToNextMajor(from: "5.3.0"))
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftBTC"),
+            name: "SwiftBTC",
+            dependencies: [
+                .product(name: "BigInt", package: "BigInt")
+            ]
+        ),
         .testTarget(
             name: "SwiftBTCTests",
-            dependencies: ["SwiftBTC"]),
+            dependencies: [
+                "SwiftBTC",
+                .product(name: "BigInt", package: "BigInt")
+            ]
+        ),
     ]
 )
